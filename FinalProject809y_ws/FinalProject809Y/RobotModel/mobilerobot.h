8c225a28a3ea8a0a7fd795650e63ae8d4298cf10
iskalasrinivas <46247472+iskalasrinivas@users.noreply.github.com>
2019-05-13 20:02:20 +0530

    Update mobilerobot.h
    updated

--- FinalProject809y_ws/FinalProject809Y/RobotModel/mobilerobot.h
+++ FinalProject809y_ws/FinalProject809Y/RobotModel/mobilerobot.h
@@ -20,16 +20,18 @@ public:
 	MobileRobot();
 	int x;
 	int y;
+	std::stack<robotstate::RobotState*> state_stack;
 	// methods
-	virtual std::vector<int> Up(int,int);
-	virtual std::vector<int> Down(int,int);
-	virtual std::vector<int> Right(int,int);
-	virtual std::vector<int> Left(int,int);
-	virtual void movedUp(int,int,Maze);
-	virtual void movedDown(int,int,Maze);
-	virtual void movedRight(int,int,Maze);
-	virtual void movedLeft(int,int,Maze);
-	virtual void update(std::string);     //  =0 Doubt
+	virtual std::vector<int> Up(int,int)=0;
+	virtual std::vector<int> Down(int,int)=0;
+	virtual std::vector<int> Right(int,int)=0;
+	virtual std::vector<int> Left(int,int)=0;
+	virtual void movedup()=0;
+	virtual void moveddown()=0;
+	virtual void movedright()=0;
+	virtual void movedleft()=0;
+	// update stack method
+	virtual void update(std::string);
 	virtual std::vector<int> getRobotLoc();
 	virtual ~MobileRobot(){};
 };
